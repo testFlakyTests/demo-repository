7eaef4846fe510850bcb0dd1e235b5c3cfdf88d5
sebastian-lerner <86729349+sebastian-lerner@users.noreply.github.com>
2025-08-14 09:23:14 -0400

    Update flaky.test.ts

--- src/flaky.test.ts
+++ src/flaky.test.ts
@@ -40,4 +40,24 @@ describe('Flaky Test Suite', () => {
   it('flaky test 10', () => {
     expect(Math.random().toString().length > 17).toBe(true)
   })
-}) 
+
+  it('flaky test 11', () => {
+    expect(Math.floor(Math.random() * 5) === 2).toBe(true)
+  })
+
+  it('flaky test 12', () => {
+    expect(['a', 'b', 'c'][Math.floor(Math.random() * 3)] === 'b').toBe(true)
+  })
+
+  it('flaky test 13', () => {
+    expect(Math.random() + Math.random() > 1.5).toBe(true)
+  })
+
+  it('flaky test 14', () => {
+    expect(Math.random() * 100 % 10 < 2).toBe(true)
+  })
+
+  it('flaky test 15', () => {
+    expect(new Date().getMilliseconds() % 3 === 0).toBe(true)
+  })
+})
